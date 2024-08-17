9f984a6eb7213abd263b16757fd3b23073937d4a
KINDAAbdoulLatif <kinda.abdoul.latif.14@gmail.com>
2024-08-17 13:41:50 +0200

    second

--- compo/Contact.tsx
+++
@@ -1,9 +0,0 @@
-export default function Contact() {
-  return (
-    <div className="my-64">  
-        <button className="border border-black">light</button>
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut adipisci cum soluta esse non expedita ex consequuntur accusamus tempora eveniet voluptatum officia minus, quas voluptates ab nostrum. Asperiores beatae exercitationem culpa totam error! Ea dicta soluta accusamus ullam accusantium deserunt labore ipsum nobis omnis velit. Libero ab nisi natus in?</p>
-    </div>
-  )
-}
